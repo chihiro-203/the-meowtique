cf86aef0b75774de375dc0c7c42c155b628814aa
ume <chi.nguyenthiyen.203@gmail.com>
2024-12-13 19:36:54 +0700

    Install Sanity with data created before

---
+++ sanity/schemaTypes/product.ts
@@ -0,0 +1,74 @@
+// A product includes a product sku, product name, images, price (in USD), reviews (maybe), sale percentage, description, category, material & care, measurements, origin.
+
+export default {
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    {
+      name: 'sku',
+      type: 'string',
+      title: 'Product SKU',
+    },
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name of Product',
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Product Images',
+      of: [{type: 'image'}],
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Product Slug',
+      options: {
+        source: 'name',
+      }
+    },
+    {
+        name: 'price',
+        type: 'number',
+        title: 'Price',
+    },
+    {
+        name: 'sale',
+        type: 'number',
+        title: 'Sale',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description of Product',
+    },
+    {
+      name: 'categories',
+      type: 'array',
+      title: 'Product Categories',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'category' }],
+        },
+      ],
+    },
+    {
+      name: 'material',
+      type: 'text',
+      title: 'Materials & Care',
+    },
+    {
+      name: 'measurements',
+      type: 'text',
+      title: 'Measurements',
+    },
+    {
+      name: 'origin',
+      type: 'text',
+      title: 'Origin',
+    },
+  ],
+}
